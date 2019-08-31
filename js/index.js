3b76d1930838a78ec9253c01d9a38d3cea7510c8
Charlerae <contactcharlerae@gmail.com>
2019-08-31 15:56:17 -0400

    MouseOver and KeyDown Event Listeners
    -added mouseover event listener to nav links
    -added keydown event listener to images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// MouseOver
+const navbuttons = document.querySelector(".main-navigation .nav-link");
+navbuttons.addEventListener("mouseover", function(e){
+    e.target.style.backgroundcolor = "blue";
+});
+
+// KeyDown
+let images = document.querySelectorAll(".img-content");
+
+window.addEventListener("keydown", e => {
+    if(e.keyCode === 65) {
+        images.forEach(img => {
+            img.style.transform = "rotate(180deg)";
+        });
+    }
+});
+
+// DoubleClick
+const funbuslogo = document.querySelector(".logo-heading");
+
+// funbuslogo.addEventListener("dblclick",)
